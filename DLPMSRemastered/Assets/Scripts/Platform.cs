8a1582e5257d374ff5b6538b2a43b60916c06499
lilmonix3UwU <Tbnielsen2005@gmail.com>
2024-03-12 10:17:55 +0100

    enemy begining

---
+++ DLPMSRemastered/Assets/Scripts/Platform.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    private GameObject[] platforms;
+    private GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+       // platforms = foreach (var item in collection)
+      //  {
+
+       // }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
