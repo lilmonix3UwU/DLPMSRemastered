593b0648fe117223b5be4401d22aee46276c63cf
lilmonix3UwU <Tbnielsen2005@gmail.com>
2024-04-30 10:19:46 +0200

    start af enemy ai

---
+++ DLPMSRemastered/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyAI : MonoBehaviour
+{
+    [Header("Pathfinding")]
+    public Transform target;
+    public float activationDistance;
+    public float pathUpdateSeconds;
+
+
+    [Header("Physics")]
+    public float speed = 200f;
+    public float nextWaypointDistance = 3f;
+    public float jumpNodeHeightRequirement = 0.8f;
+    public float jumpModifier = 0.3f;
+    public float jumpCheckOffset = 0.1f;
+}
