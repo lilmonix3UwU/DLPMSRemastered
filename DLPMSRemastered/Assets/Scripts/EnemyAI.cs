dbd8aa80406741737bb56ee935840bbbbdac761a
lilmonix3UwU <Tbnielsen2005@gmail.com>
2024-05-14 12:26:48 +0200

    smertefuld enemy AI

--- DLPMSRemastered/Assets/Scripts/EnemyAI.cs
+++ DLPMSRemastered/Assets/Scripts/EnemyAI.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Pathfinding;
+using System.Security.Cryptography;
 
 public class EnemyAI : MonoBehaviour
 {
@@ -17,6 +18,7 @@ public class EnemyAI : MonoBehaviour
     public float jumpNodeHeightRequirement = 0.8f;
     public float jumpModifier = 0.3f;
     public float jumpCheckOffset = 0.1f;
+    public int jumpChance;
 
     [Header("Costum Behavior")]
     public bool followEnabled = true;
@@ -31,7 +33,92 @@ public class EnemyAI : MonoBehaviour
 
     private void Start()
     {
-        
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+
+        InvokeRepeating("UpdatePath", 0f, pathUpdateSeconds);
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (TargetInDistance() && followEnabled)
+        {
+            PathFollow();
+        }
+    }
+
+    private void UpdatePath()
+    {
+        if (TargetInDistance() && followEnabled && seeker.IsDone())
+        {
+            seeker.StartPath(rb.position, target.position, OnPathComplete);
+        }
+    }
+
+    private void PathFollow()
+    {
+        Debug.Log("pp");
+        if (path == null)
+        {
+            return;
+        }
+        Debug.Log("peepee");
+        if (currentWaypoint >= path.vectorPath.Count)
+        {
+            return;
+        }
+        Debug.Log("poopoo");
+        isGrounded = Physics2D.Raycast(transform.position, -Vector3.up, GetComponent<Collider2D>().bounds.extents.y + jumpCheckOffset);
+       
+        Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
+        Vector2 force = direction * speed * Time.deltaTime;
+
+        if (jumpEnabled && isGrounded)
+        {
+            jumpChance = Random.Range(1, 4);
+            if (direction.y > jumpNodeHeightRequirement && jumpChance == 1)
+            {
+                rb.AddForce(Vector2.up * speed * jumpModifier);
+            }
+        }
+
+
+        rb.AddForce(force);
+
+
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+        if (distance < nextWaypointDistance)
+        {
+            currentWaypoint++;
+        }
+
+        if (directionLookEnabled)
+        {
+            if (rb.velocity.x > 0.05f)
+            {
+                transform.localScale = new Vector3(-1f * Mathf.Abs(transform.localScale.x), transform.localScale.z);
+            }
+            else if (rb.velocity.x > -0.05f)
+            {
+                transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.z);
+            }
+
+        }
+    }
+
+    private bool TargetInDistance()
+    {
+        return Vector2.Distance(transform.position, target.transform.position) < activationDistance;
+    }
+
+    private void OnPathComplete(Path p)
+    {
+        if (!path.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
     }
 }
 
