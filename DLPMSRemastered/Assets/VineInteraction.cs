84a3685b707cc5b7d7ef24491256bf41a0d01b7d
lilmonix3UwU <Tbnielsen2005@gmail.com>
2024-09-19 22:29:26 +0200

    vimes ray

--- DLPMSRemastered/Assets/VineInteraction.cs
+++ DLPMSRemastered/Assets/VineInteraction.cs
@@ -6,9 +6,10 @@ public class VineInteraction : MonoBehaviour
 {
     private void Update()
     {
-        if (Physics2D.Raycast(gameObject.transform.position, gameObject.transform.forward, 2))
+        if (Physics2D.Raycast(gameObject.transform.position, gameObject.transform.forward, 2, 13))
         {
-            
+            RaycastHit2D hit = Physics2D.Raycast(gameObject.transform.position, gameObject.transform.forward, 2, 13);
+
         }
     }
 
